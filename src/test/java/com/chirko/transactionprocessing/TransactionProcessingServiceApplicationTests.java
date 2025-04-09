39d6a29af3733516547eb7f2acf7e9e1a96b04a1
Konstantin Chirko <kostyachirko375@gmail.com>
2025-04-09 20:18:07 +0300

    Initial commit

---
+++ src/test/java/com/chirko/transactionprocessing/TransactionProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chirko.transactionprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionProcessingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
